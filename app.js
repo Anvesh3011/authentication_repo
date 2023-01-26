870d8069c4921be797cdd992cfd45b2a3aad2c93
Anvesh1130 <kankanala.3333@gmail.com>
2023-01-26 23:09:06 +0530

    this is the final version of the application with level6 authentication which is OAUTH or open authorisation or thirdparty authorisation like google facebook

--- app.js
+++ app.js
@@ -9,10 +9,9 @@ const encrypt = require("mongoose-encryption");
 const session = require('express-session'); //express-session is required
 const passport = require("passport"); //passport is require
 const passportLocalMongoose = require("passport-local-mongoose"); //passport-local-mongoose is require
-const {
-    initialize
-} = require("passport");
 
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 
 
@@ -41,24 +40,61 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB", {
 //TODO
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String,
+    secret: String
 
 });
 
 //enables passportLocalMongoose
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 
 const User = new mongoose.model("User", userSchema);
 //used passport to createstragey and to serialize and deserialize
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+passport.use(new GoogleStrategy({
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/auth/google/secrets",
+        userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
+    },
+    function (accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({
+            googleId: profile.id
+        }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
 app.get('/', (req, res) => {
     res.render("home");
 });
+app.get('/auth/google',
+    passport.authenticate('google', {
+        scope: ["profile"]
+    }));
+
+app.get('/auth/google/secrets',
+    passport.authenticate('google', {
+        failureRedirect: '/login'
+    }),
+    function (req, res) {
+        // Successful authentication, redirect to secrets.
+        res.redirect('/secrets');
+    });
+
+
 app.get('/login', (req, res) => {
     res.render("login");
 });
@@ -66,12 +102,29 @@ app.get('/register', (req, res) => {
     res.render("register");
 });
 app.get("/secrets", function (req, res) {
+    User.find({
+        "secret": {
+            $ne: null
+        }
+    }, function (err, foundUsers) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUsers) {
+                res.render("secrets", {
+                    usersWithSecrets: foundUsers
+                });
+            }
+        }
+    });
+
+});
+app.get("/submit", function (req, res) {
     if (req.isAuthenticated()) {
-        res.render("secrets");
+        res.render("submit");
     } else {
         res.redirect("/login");
     }
-
 });
 app.get("/logout", function (req, res) {
     req.logout(function (err) {
@@ -84,6 +137,7 @@ app.get("/logout", function (req, res) {
     });
 
 });
+
 app.post('/register', (req, res) => {
     User.register({
         username: req.body.username
@@ -114,6 +168,23 @@ app.post('/login', function (req, res) {
             });
         }
     });
+
+});
+app.post('/submit', function (req, res) {
+    const submittedSecret = req.body.secret;
+    User.findById(req.user.id, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(function () {
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    });
+
 
 });
 app.listen(3000, () => {
